358e37297df6350616dc54445ed5806be32d6bf9
Nick Becker <root@Isabellas-MacBook-Pro.local>
2014-12-10 19:03:06 -0800

    Nick Becker's final Lambda Magic Assignment

--- Students/NickBecker/lambda_magic.py
+++ Students/NickBecker/lambda_magic.py
@@ -2,7 +2,11 @@ def func_builder(num_size):
     """Create a list of functions such that they add an inputted number to their index"""
     return [lambda inp1, i = inp2: i + inp1 for inp2 in range(num_size)]
 
-print func_builder(90)
+q = func_builder(100)
+
+print q[5](5)
 
 if __name__ == "__main__":
-    #FIX THIS AS WELL AS THE INPUTS
+    assert isinstance(q[4], type(func_builder))
+    assert q[5](5) == 10
+    assert q[10](0) == 10
