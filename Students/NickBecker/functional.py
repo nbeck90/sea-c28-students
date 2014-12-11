d80b2c7137481d838244c4ed4b21aef7b7697906
Nick Becker <root@Isabellas-MacBook-Pro.local>
2014-12-10 19:05:16 -0800

    Nick Becker's Functional File assignment

--- Students/NickBecker/functional.py
+++ Students/NickBecker/functional.py
@@ -1,25 +1,28 @@
+#!/usr/bin/env python
 import codecs
 import sys
 
-my_file = sys.argv[1]
+if __name__ == "__main__":
 
+    my_file = sys.argv[1]
 
-def cleaning(my_file):
-    for lines in my_file:
-        my_file.write(lines.strip())
+    def cleaning(my_file):
+        for lines in my_file:
+            my_file.write(lines.strip())
 
-while True:
-    selection = raw_input("Please type 'N' to write to a new file or 'O' to write over the current file")
+    while True:
+        selection = raw_input("Please type 'N' to write to a new file or 'O' to write over the current file: ")
 
-    if selection.lower() == "n":
-        print "Making a new file without leading/trailing whitespace"
-        newfile = codecs.open("%s_clean.txt" % str(my_file), 'w')
-        cleaning(codecs.open(my_file))
+        if selection.lower() == "n":
+            print "Making a new file without leading/trailing whitespace"
+            newfile = codecs.open("%s_clean.txt" % str(my_file), 'w')
+            cleaning(codecs.open(my_file))
 
-    elif selection.lower() == "o":
-        print "Deleting trailing/leading whitespace of he current file"
-        cleanfile = codecs.open(my_file, 'r+')
-        cleanfile.seek(0)
-        cleaning(cleanfile)
-    else:
-        print "Please select a valid option"
+        elif selection.lower() == "o":
+            print "Deleting trailing/leading whitespace of he current file"
+            cleanfile = codecs.open(my_file, 'r+')
+            cleanfile.seek(0)
+            cleaning(cleanfile)
+            break
+        else:
+            print "Please select a valid option"
