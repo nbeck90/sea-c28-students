5ce972d77274fcc3676d7173135a3002ac8f1d5b
Nick Becker <root@Isabellas-MacBook-Pro.local>
2014-12-11 23:29:04 -0800

    Resubmission of Nick Becker's ListLab Assignment

--- Students/NickBecker/list_lab.py
+++ Students/NickBecker/list_lab.py
@@ -1,150 +1,111 @@
-# Part one of list_lab: adding to lists and choosing answers
-fruits = ["Apples", "Pears", "Oranges", "Peaches"]
+if __name__ == "__main__":
 
-print fruits
+    # Part one of list_lab: adding to lists and choosing answers
+    fruits = ["Apples", "Pears", "Oranges", "Peaches"]
 
-input_fruit1 = raw_input("Choose a fruit to add to the list: ")
+    print fruits
 
-fruits.append(input_fruit1)
+    input_fruit1 = raw_input("Choose a fruit to add to the list: ")
 
-print "You now have these fruits"
+    fruits.append(input_fruit1)
 
-print fruits
+    print "You now have these fruits"
 
-while True:
-    fruit_query = raw_input("Pick a number of one of your fruits: ")
-    if fruit_query.isdigit() is False:
-        print "Please enter an integer"
-    elif int(fruit_query) == 0:
-        print "You have more than 0 fruits, enter a number!"
-    elif int(fruit_query) > len(fruits):
-        print "Your number is too high!"
-    else:
-        print fruit_query
-        print fruits[int(fruit_query) - 1]
-        break
+    print fruits
 
-print fruits
+    while True:
+        fruit_query = raw_input("Pick a number of one of your fruits: ")
+        if fruit_query.isdigit() is False:
+            print "Please enter an integer"
+        elif int(fruit_query) == 0:
+            print "You have more than 0 fruits, enter a number!"
+        elif int(fruit_query) > len(fruits):
+            print "Your number is too high!"
+        else:
+            print fruit_query
+            print fruits[int(fruit_query) - 1]
+            break
 
-input_fruit2 = raw_input("Choose another fruit to add to the list: ")
+    print fruits
 
-fruits2 = [input_fruit2]
-fruits += fruits2
-print fruits
+    input_fruit2 = raw_input("Choose another fruit to add to the list: ")
 
-input_fruit3 = raw_input("Choose a third fruit!: ")
+    fruits2 = [input_fruit2]
+    fruits += fruits2
+    print fruits
 
-fruits.insert(0, input_fruit3)
-print fruits
+    input_fruit3 = raw_input("Choose a third fruit!: ")
 
-print "Displaying fruits that start with 'P'"
+    fruits.insert(0, input_fruit3)
+    print fruits
 
-for item in fruits:
-    if item[0] == "P":
-        print item
-    elif item[0] == "p":
-        print item
-    else:
-        pass
+    print "Displaying fruits that start with 'P'"
 
-# Part 2 of list_lab: deleting items from lists
+    for item in fruits:
+        if item[0] == "P":
+            print item
+        elif item[0] == "p":
+            print item
+        else:
+            pass
 
-print fruits
+    # Part 2 of list_lab: deleting items from lists
 
-print "Deleting the last fruit"
+    print fruits
 
-fruits.pop()
+    print "Deleting the last fruit"
 
-print fruits
+    fruits.pop()
 
-fruit_del = raw_input("Pick a fruit to get rid of: ")
+    print fruits
 
-for items in fruits:
-    if items == fruit_del:
-        fruits.remove(fruit_del)
-    else:
-        pass
+    fruit_del = raw_input("Pick a fruit to get rid of: ")
 
-print fruits
+    for items in fruits:
+        if items == fruit_del:
+            fruits.remove(fruit_del)
+        else:
+            pass
 
-#Part 3 of list_lab (Do you like?: yes or no)
+    print fruits
 
-print fruits
+    #Part 3 of list_lab (Do you like?: yes or no)
 
-while True:
-    apples_ans = raw_input("Do you like apples?: ")
-    if apples_ans.lower() == "yes":
-        print "Great!"
-        pass
-        break
-    elif apples_ans.lower() == "no":
-        print "That's a shame"
-        fruits.remove("Apples")
-        break
-    else:
-        print "Please answer yes or no"
+    print fruits
 
-print fruits
+    new_fruits = []
 
-while True:
-    pears_ans = raw_input("Do you like pears?: ")
-    if pears_ans.lower() == "yes":
-        print "Great!"
-        pass
-        break
-    elif pears_ans.lower() == "no":
-        print "That's a shame"
-        fruits.remove("Pears")
-        break
-    else:
-        print "Please answer yes or no"
+    for items in fruits:
+        while True:
+            fruit_answer = raw_input("Do you like %s?: " % (items))
+            if fruit_answer.lower() == "yes":
+                new_fruits.append(items)
+                print "Great!"
+                pass
+                break
+            elif fruit_answer.lower() == "no":
+                print "That's a shame"
+                break
+            else:
+                print "Please answer yes or no"
+        fruits = new_fruits
 
-print fruits
+    print fruits
 
-while True:
-    oranges_ans = raw_input("Do you like oranges?: ")
-    if oranges_ans.lower() == "yes":
-        print "Great!"
-        pass
-        break
-    elif oranges_ans.lower() == "no":
-        print "That's a shame"
-        fruits.remove("Oranges")
-        break
-    else:
-        print "Please answer yes or no"
+    #Part 4 of list_lab: copying lists, reversing letters, deleting
 
-print fruits
+    fruits_copy = list(fruits)
 
-while True:
-    peaches_ans = raw_input("Do you like peaches?: ")
-    if peaches_ans.lower() == "yes" and "Peaches" is in fruits:
-        print "Great!"
-        pass
-        break
-    elif peaches_ans.lower() == "no":
-        print "That's a shame"
-        fruits.remove("Peaches")
-        break
-    else:
-        print "Please answer yes or no"
+    def rev_list(mylist):
+        new_list = []
+        for items in mylist:
+            items = items[::-1]
+            new_list.append(items)
+        print new_list
 
-print fruits
+    print rev_list(fruits_copy)
+    print fruits
 
-#Part 4 of list_lab: copying lists, reversing letters, deleting
-
-fruits_copy = list(fruits)
-
-def rev_list(mylist):
-    new_list = []
-    for items in mylist:
-        items = items[::-1]
-        new_list.append(items)
-    print new_list
-
-print rev_list(fruits_copy)
-print fruits
-
-fruits.pop()
-print fruits
-print rev_list(fruits_copy)
+    fruits.pop()
+    print fruits
+    print rev_list(fruits_copy)
