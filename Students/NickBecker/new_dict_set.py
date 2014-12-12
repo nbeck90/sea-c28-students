e13fe917fe32a164c22eac6c60f29927b8997a19
Nick Becker <root@Isabellas-MacBook-Pro.local>
2014-12-11 20:09:46 -0800

    Adding Nick's fixed new_dict_set.py file

--- Students/NickBecker/new_dict_set.py
+++ Students/NickBecker/new_dict_set.py
@@ -37,3 +37,11 @@ s4 = {nums for nums in range(21) if nums % 4 == 0}
 print s4
 
 
+def int_mod(new_mod=4):
+    new_list = []
+    for items in range(1, new_mod):
+        new_list.append({nums for nums in range(21) if nums % items == 0})
+    return new_list
+
+
+print int_mod(5)
