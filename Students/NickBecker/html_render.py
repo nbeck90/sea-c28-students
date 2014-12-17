571a4adec2f02626376d692ca472e79e6af54d95
Nick Becker <root@Isabellas-MacBook-Pro.local>
2014-12-16 18:33:33 -0800

    Adding Nick Becker's newest(final) version of the html_render.py

--- Students/NickBecker/html_render.py
+++ Students/NickBecker/html_render.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
-
-
 class Element(list):
-
+    """Creates all elements for the HTML rendering"""
     indent = "    "
 
     def __init__(self, content=None, **kwargs):
@@ -10,152 +7,123 @@ class Element(list):
             self.content = []
         else:
             self.content = [content]
-        if kwargs is None:
-            self.attributes = None
-        else:
-            self.attributes = kwargs
+        self.attribute = kwargs
         self.html_tag = ""
-        self.ind = "    "
 
     def append(self, words=None):
         self.content.append(words)
 
-    def render(self, file_out, ind=""):
-        if self.attributes:
-            attrib_list = [" "]
-            for attribs in self.attributes:
-                attrib_list.append('{}="{}"'.format(attribs,
-                                                    self.attributes[attribs]))
-            additions = " ".join(attrib_list)
-        else:
-            additions = ""
-        file_out.write(
-            "{}{}<{}{}>\n".format(ind,
-                                  self.indent,
-                                  self.html_tag,
-                                  additions)
-                      )
-        for line in self.content:
+    def render(self, file_out, ind="    "):
+        attribute_list = []
+        if self.attribute:
+            for key, value in self.attribute.items():
+                attribute_list.append("{}={}".format(key, value))
+        file_out.write("{}<{} {}>\n".format(ind,
+                                            self.html_tag,
+                                            " ".join(attribute_list)))
+        for text in self.content:
             try:
-                line.render(file_out, ind + self.indent)
+                text.render(file_out, self.indent + ind)
             except AttributeError:
-                file_out.write("{}{}{}\n".format(ind,
-                                                 self.indent,
-                                                 line)
-                              )
-        file_out.write("{}{}</{}>\n".format(ind,
-                                            self.indent,
-                                            self.html_tag)
-                      )
+                file_out.write("{}{}\n".format(ind,
+                                               text))
 
-
-# reload(hr)
-# e1 = hr.Element()
-# e1.append("sometext")
-# e1.render()
-
-
-class OneLineTag(Element):
-    def __init__(self, content=None, **kwargs):
-        super(OneLineTag, self).__init__(content, **kwargs)
-
-    def render(self, file_out, ind=u""):
-        file_out.write(
-            "{}{}<{}> {} </{}>\n".format(ind,
-                                         self.indent,
-                                         self.html_tag,
-                                         self.content,
-                                         self.html_tag
-                                        )
-                      )
-
-
-class SelfClosingTag(Element):
-    def render(self, file_out, ind=""):
-        file_out.write("{}{}<{} />\n".format(ind,
-                                            self.indent,
-                                            self.html_tag)
-                      )
-
-
-class P(Element):
-    def __init__(self, content=None, **kwargs):
-        super(P, self).__init__(content, **kwargs)
-        self.html_tag = "p"
+        file_out.write("{}</{}>\n".format(ind, self.html_tag))
 
 
 class Body(Element):
+    """Creates a body tag"""
     def __init__(self, content=None, **kwargs):
         super(Body, self).__init__(content, **kwargs)
         self.html_tag = "body"
 
 
-class List(Element):
-    def __init__(self, content=None, **kwargs):
-        super(List, self).__init__(content, **kwargs)
-        self.html_tag = "li"
-
-
 class Html(Element):
+    """Creates an HTML tag as well as a doctype"""
     def __init__(self, content=None, **kwargs):
         super(Html, self).__init__(content, **kwargs)
         self.html_tag = "html"
 
-    def render(self, file_out, ind=""):
+    def render(self, file_out, ind="    "):
+        attribute_list = []
+        if self.attribute:
+            for key, value in self.attribute.items():
+                attribute_list.append("{}={}".format(key, value))
         file_out.write("<!DOCTYPE html>\n")
-        if self.attributes:
-            attrib_list = [" "]
-            for attribs in self.attributes:
-                attrib_list.append('{}="{}"'.format(attribs,
-                                                    self.attributes[attribs]))
-            additions = " ".join(attrib_list)
-        else:
-            additions = ""
-        file_out.write(
-            "{}{}<{}{}>\n".format(ind,
-                                  self.indent,
-                                  self.html_tag,
-                                  additions)
-                      )
-        for line in self.content:
+        file_out.write("{}<{} {}>\n".format(ind,
+                                            self.html_tag,
+                                            " ".join(attribute_list)))
+        for text in self.content:
             try:
-                line.render(file_out, ind + self.indent)
+                text.render(file_out, self.indent + ind)
             except AttributeError:
-                file_out.write("{}{}{}\n".format(ind,
-                                                 self.indent,
-                                                 line)
-                              )
-        file_out.write("{}{}</{}>\n".format(ind,
-                                            self.indent,
-                                            self.html_tag)
-                      )
+                file_out.write("{}{}\n".format(ind,
+                                               text))
+
+        file_out.write("{}</{}>\n".format(ind, self.html_tag))
 
 
-class Title(OneLineTag):
+class P(Element):
+    "Creates a paragraph tag"""
     def __init__(self, content=None, **kwargs):
-        super(Title, self).__init__(content, **kwargs)
-        self.html_tag = "title"
+        super(P, self).__init__(content, **kwargs)
+        self.html_tag = "p"
 
 
 class Head(Element):
+    """Creates a header tag"""
     def __init__(self, content=None, **kwargs):
         super(Head, self).__init__(content, **kwargs)
         self.html_tag = "head"
 
 
+class OneLineTag(Element):
+    """Creates a tag that is positioned all on one line"""
+    def render(self, file_out, ind="    "):
+        file_out.write("{}<{}>".format(ind, self.html_tag))
+        for text in self.content:
+            try:
+                text.render(file_out, self.indent + ind)
+            except AttributeError:
+                file_out.write("{}".format(text))
+
+        file_out.write("</{}>\n".format(self.html_tag))
+
+
+class Title(OneLineTag):
+    """Creates a title tag"""
+    def __init__(self, content=None, **kwargs):
+        super(Title, self).__init__(content, **kwargs)
+        self.html_tag = "title"
+
+
+class SelfClosingTag(Element):
+    """Creates a tag that closes itself"""
+    def __init__(self, content=None, **kwargs):
+        super(SelfClosingTag, self).__init__(content, **kwargs)
+
+    def render(self, file_out, ind=""):
+        file_out.write("{}{}<{} />\n".format(ind,
+                                             self.indent,
+                                             self.html_tag))
+
+
 class Hr(SelfClosingTag):
+    """Adds in a horizontal line"""
     def __init__(self, content=None, **kwargs):
         super(Hr, self).__init__(content, **kwargs)
         self.html_tag = "hr"
 
 
 class Br(SelfClosingTag):
+    """Adds in a line break"""
     def __init__(self, content=None, **kwargs):
         super(Br, self).__init__(content, **kwargs)
         self.html_tag = "br"
 
 
 class A(Element):
+    """Creates a link to a webpage, a.k.a, an anchor"""
     def __init__(self, link=None, content=None):
         new_link = {"href": '{}'.format(link)}
         super(A, self).__init__(content, **new_link)
@@ -163,24 +131,28 @@ class A(Element):
 
 
 class Ul(Element):
+    """Creates an unordered list"""
     def __init__(self, content=None, **kwargs):
         super(Ul, self).__init__(content, **kwargs)
         self.html_tag = "ul"
 
 
 class Li(Element):
+    """Creates an item in a list"""
     def __init__(self, content=None, **kwargs):
         super(Li, self).__init__(content, **kwargs)
         self.html_tag = "li"
 
 
 class H(Element):
+    """Creates a header off of a given size"""
     def __init__(self, size, content=None, **kwargs):
         super(H, self).__init__(content, **kwargs)
         self.html_tag = "H{}".format(size)
 
 
 class Meta(SelfClosingTag):
+    """Sets the encoding of the HTML output"""
     def __init__(self, content=None, **kwargs):
         super(Meta, self).__init__(content, **kwargs)
         self.html_tag = 'meta charset="UTF-8"'
